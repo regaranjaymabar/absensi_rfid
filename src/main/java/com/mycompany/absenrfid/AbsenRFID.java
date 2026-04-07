c72c51aa710edd6a577097faca2f28c393bc2ef0
regaranjaymabar <rhegaraa5x@gmail.com>
2026-04-07 12:35:59 +0700

    komit pertama

---
+++ src/main/java/com/mycompany/absenrfid/AbsenRFID.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.absenrfid;
+
+/**
+ *
+ * @author USER
+ */
+public class AbsenRFID {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
